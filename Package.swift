f181600cc1f9ca1e70a5176013e271a8601497e2
Ryan Grimm <ryan@ryangrimm.com>
2018-07-12 09:18:54 -0700

    A start to an example project.

--- Package.swift
+++ Package.swift
@@ -8,12 +8,9 @@ let package = Package(
 
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Elasticsearch"]),
+        .target(name: "App", dependencies: ["Vapor", "Elasticsearch"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,10 @@
-import FluentSQLite
 import Vapor
+import Elasticsearch
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(ElasticsearchProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -17,17 +17,15 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    var esConfig = ElasticsearchClientConfig()
+    esConfig.hostname = "localhost"
+    esConfig.port = 9200
+    
+    // Configure an Elasticsearch database
+    let es = try ElasticsearchDatabase(config: esConfig)
 
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: es, as: .elasticsearch)
     services.register(databases)
-
-    /// Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
-    services.register(migrations)
-
 }
